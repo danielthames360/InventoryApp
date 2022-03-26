db631a98fd1f8de3cf10cf985835d3a031765012
danielthames360 <danielthames360@gmail.com>
2022-03-26 14:47:05 -0400

    Adding transitions between components

--- src/modules/dashboard/layouts/DashboardLayout.vue
+++ src/modules/dashboard/layouts/DashboardLayout.vue
@@ -3,11 +3,15 @@
     class="relative z-0 flex justify-between h-screen max-h-[-webkit-fill-available] mx-auto antialiased"
   >
     <NavBar :toggleNavSide="toggleNav" />
-    <div class="relative w-full p-10 sm:w-12/12 -z-50 h-fit" @click="toggleNav = !toggleNav">
+    <div class="relative w-full p-10 sm:w-12/12 -z-50 h-fit min-h-[80vh]" @click="toggleNav = !toggleNav">
       <div class="flex items-center justify-center">
         <h1 class="text-center dark:text-light text-dark-300">
           {{ $t('dashboard.text') }}
-          <router-view></router-view>
+          <router-view v-slot="{ Component }">
+            <transition name="fade" mode="out-in">
+              <component :is="Component" />
+            </transition>
+          </router-view>
         </h1>
       </div>
     </div>
@@ -25,3 +29,15 @@ const NavBar = defineAsyncComponent(() => import('../components/NavBar.vue'))
 
 </script>
  
+
+ <style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
